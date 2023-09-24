9de1ca59765643e70693872e896b9855ec4a42c8
unknown <dimassetyonugroho30@gmail.com>
2023-09-24 17:43:29 +0700

    init app

---
+++ myFirstJavaGithub08.java
@@ -0,0 +1,10 @@
+public class myFirstJavaGithub08 {
+    public static void main(String[] args) {
+        System.out.println("Hello github");
+
+        float satu = 4.5f;
+        short dua = (short) satu;
+        System.out.println(dua);
+
+    }
+}
